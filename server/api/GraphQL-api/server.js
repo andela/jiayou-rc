c491314a0f84876159015718ef33e96ff61e1d64
andela-uenyingwa <uloaku.enyingwa@andela.com>
2017-02-27 16:53:54 +0100

    feature(restful-api): create endpoints for products and shops [#140202767]

--- server/api/GraphQL-api/server.js
+++ server/api/GraphQL-api/server.js
@@ -6,16 +6,47 @@ import axios from "axios";
 const app = express();
 const PORT = 8000;
 
-// Get products
-// app.get("/api/products", (req, res) => {
-//   axios.get(`http://${req.headers.host}/graphql`, )
-// });
-
 app.use("/graphql", GraphQLHTTP({
   schema,
   graphiql: true,
   pretty: true
 }));
+
+// Get products
+app.get("/api/products", (request, response) => {
+  axios.post(`http://${request.headers.host}/graphql`,
+    {
+      query: "{products {title _id vendor price inventoryQuantity}}"},
+    {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then((res) => {
+        response.status(200).json(res.data);
+      })
+      .catch((error) => {
+        response.status(400).send(error);
+      });
+});
+
+app.get("/api/shops", (request, response) => {
+  axios.post(`http://${request.headers.host}/graphql`,
+    {
+      query: "{shops {name _id description email status}}"
+    },
+    {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then((res) => {
+      response.status(200).json(res.data);
+    })
+    .catch((error) => {
+      response.status(400).send(error);
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`GraphQL server started on port ${PORT}`);
