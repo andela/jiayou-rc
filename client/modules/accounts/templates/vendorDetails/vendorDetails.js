904b020d3d3f530df1aedf41dbbe0a34c47d666d
I-Diva <olayemifolakemi@yahoo.com>
2017-03-17 07:18:43 +0100

    style(format): format conditional statements

--- client/modules/accounts/templates/vendorDetails/vendorDetails.js
+++ client/modules/accounts/templates/vendorDetails/vendorDetails.js
@@ -2,7 +2,7 @@ import { Meteor } from "meteor/meteor";
 import { Template } from "meteor/templating";
 import { Accounts } from "/lib/collections";
 Template.vendorDetails.helpers({
-  getVendorDetails: function () {
+  getVendorDetails: () => {
     const userDetails = Accounts.findOne(Meteor.userId);
     return userDetails.profile.vendorDetails[0];
   }
